cd2a3fc53706005617352413f061f3e9620f581d
sarahmarquezf <sarah.marquez@correo.unimet.edu.ve>
2025-03-17 00:36:13 -0400

    functions_results_management.py

--- functions_results_management.py
+++ functions_results_management.py
@@ -34,10 +34,68 @@ def find_experiment(experiments, recipes): # búsqueda de experimento para valid
 def validate_results(experiment, recipes): # valida el resultado
     for recipe in recipes:
         if recipe.id == experiment.recipe:
-            result = recipe.valores_a_medir
+            result = calculation(recipe)
             if result == None:
                 return "\nHubo algún error en la fórmula."
             elif result >= recipe.values[0].minimum and result <= recipe.values[0].maximum:
                 return f"\nEl resultado es {round(result, 3)} y se encuentra dentro de los parametros establecidos 'mínimo: {recipe.values[0].minimum}' - 'máximo: {recipe.values[0].maximum}'."
             else:
                 return f"\nEl resultado es {round(result, 3)} y no se encuentra dentro de los parametros establecidos 'mínimo: {recipe.values[0].minimum}' - 'máximo: {recipe.values[0].maximum}'."
+
+def calculation(recipe): # calcula el resultado y lo compara con los parametros
+    allowed_functions = {"log": math.log10, "sqrt": math.sqrt, "sin": math.sin, "cos": math.cos, "tan": math.tan, "exp": math.exp, "pow": math.pow}
+    
+    if len(recipe.values) == 1: # caso 1: Solo hay una opción para validar
+        if "=" not in recipe.values[0].formula:
+            formula = input(f"\n{recipe.values[0].formula}...\n\n>> ")
+            while formula == "":
+                formula = input(f"\n{recipe.values[0].formula}...\n\n>> ")
+
+        else:
+            formula = input(f"\nIngrese la formula '{recipe.values[0].formula}' sustituyendo los valores correspondientes y en ese mismo formato...\n\n>> ")
+            while formula == "":
+                formula = input(f"\Ingrese la formula '{recipe.values[0].formula}' sustituyendo los valores correspondientes y en ese mismo formato...\n\n>> ")
+
+        try:
+            result = eval(formula.replace("^", "**"), {"__builtins__": None}, allowed_functions)
+            return result
+        except:
+            return None
+    
+    elif len(recipe.values) > 1: # caso 2: Hay más de una opción para validar
+        idx = 0
+        values_dict = {}
+        print("\nSeleccione los valores a medir...\n")
+        for v in recipe.values:
+            print(f"{idx + 1}. {v.name}.")
+            values_dict[idx] = v
+            idx += 1
+
+        option = input("\n>> ")
+
+        while not option.isnumeric() or int(option) not in range(1, idx + 1):
+            idx = 0
+            print("\nSeleccione algún valor a medir válido...\n")
+            for v in recipe.values:
+                print(f"{idx + 1}. {v.name}.")
+                idx += 1
+            option = input("\n>> ")
+
+        recipe_s = values_dict[int(option)-1]
+
+        if "(" in recipe_s.formula or "=" in recipe_s.formula:
+            formula_to_show = re.sub(r"\[(.*?)\]", r"(\1)", recipe_s.formula)
+            formula = input(f"\nIngrese la formula '{formula_to_show}' sustituyendo los valores correspondientes y en ese mismo formato...\n\n>> ")
+            while formula == "":
+                formula = input(f"\nIngrese la formula '{formula_to_show}' sustituyendo los valores correspondientes y en ese mismo formato...\n\n>> ")
+
+        else:
+            formula = input(f"\n{recipe_s.formula}...\n\n>> ")
+            while formula == "":
+                formula = input(f"\n{recipe_s.formula}...\n\n>> ")
+
+        try:
+            result = eval(formula.replace("^", "**"), {"__builtins__": None}, allowed_functions)
+            return result
+        except:
+            return None
